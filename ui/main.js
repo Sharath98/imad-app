0996aeb7ad4c002a8276854a95f21fe75cf611d1
Sharath98 <sguddad98@gmail.com>
2017-08-21 12:29:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,6 @@ console.log('Loaded!');
 
 var element= document.getElementById('main-text');
 
-element.innerHTML = 'New Value';
-
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
